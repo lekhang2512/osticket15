de813c3e893e5f12c749c7a6919e15548e0eb28c
Le Bao Khang <lebaokhang@omegatheme.com>
2022-06-22 16:03:07 +0700

    local

---
+++ setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.5.1.min.js?6bd7884"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?6bd7884"></script>
+    <script type="text/javascript" src="js/setup.js?6bd7884"></script>
+    <script type="text/javascript" src="js/tips.js?6bd7884"></script>
+</body>
+</html>
